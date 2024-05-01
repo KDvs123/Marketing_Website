22137d02c1f5e121ebbf82d781a86b91aa5a9a28
vihanga supasan <119438971+KDvs123@users.noreply.github.com>
2024-05-01 23:45:27 +0530

    Update Roadmap.jsx

--- src/components/Roadmap.jsx
+++ src/components/Roadmap.jsx
@@ -9,7 +9,7 @@ import { Gradient } from "./design/Roadmap";
 const Roadmap = () => (
   <Section className="overflow-hidden" id="roadmap">
     <div className="container md:pb-10">
-      <Heading tag="Ready to get started" title="What we’re working on" />
+      <Heading tag="Ready to get started" title="Discover the Exciting Features of Our Application" />
 
       <div className="relative grid gap-6 md:grid-cols-2 md:gap-4 md:pb-[7rem]">
         {roadmap.map((item) => {
@@ -69,7 +69,7 @@ const Roadmap = () => (
       </div>
 
       <div className="flex justify-center mt-12 md:mt-15 xl:mt-20">
-        <Button href="/roadmap">Get The App</Button>
+        <Button href="https://drive.google.com/file/d/1GTT34h6hBVJU0-duHx5MjuwiAqAgZFm9/viewp">Get The App</Button>
       </div>
     </div>
   </Section>
