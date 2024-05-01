2789bd1a87d21939296ebe87c99fd1db9bbba4cb
vihanga supasan <119438971+KDvs123@users.noreply.github.com>
2024-05-01 22:17:31 +0530

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -40,7 +40,7 @@ export const navigation = [
   {
     id: "0",
     title: "Features",
-    url: "#features",
+    url: "#roadmap",
   },
   {
     id: "1",
@@ -48,24 +48,14 @@ export const navigation = [
     url: "#pricing",
   },
   {
-    id: "2",
-    title: "How to use",
-    url: "#how-to-use",
-  },
-  {
     id: "3",
-    title: "Roadmap",
-    url: "#roadmap",
-  },
-  {
-    id: "4",
-    title: "New account",
+    title: "Teahub",
     url: "#signup",
     onlyMobile: true,
   },
   {
-    id: "5",
-    title: "Sign in",
+    id: "4",
+    title: "Get The App ",
     url: "#login",
     onlyMobile: true,
   },
