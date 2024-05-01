a84eafb7b626ca64e98bdb635ddd04b548b9ff81
vihanga supasan <119438971+KDvs123@users.noreply.github.com>
2024-05-01 22:22:58 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -71,10 +71,10 @@ const Header = () => {
           href="#signup"
           className="button hidden mr-8 text-n-1/50 transition-colors hover:text-n-1 lg:block"
         >
-          New account
+          Welcome to TeaHub
         </a>
         <Button className="hidden lg:flex" href="#login">
-          Sign in
+          Get The App
         </Button>
 
         <Button
